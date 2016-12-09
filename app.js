82b012a1bf4b566db2aeade018276610cb397020
Alexander R. Torrijos <artorrijos@gmail.com>
2016-12-09 11:31:01 +0000

    Hello world express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(request, response) {
+
+response.send("Hello, world!");
+
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+
+console.log("Express app started on port 3000.");
+
+});
